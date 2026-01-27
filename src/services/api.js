778380b9d2ef43c4e7088ad440ddb901d520d749
sagar singh <sagarsinghkoranga@gmail.com>
2026-01-27 18:31:06 +0530

    add env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://ecommerce-backend-alnr.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export  const api = axios.create({
   baseURL: API_URL,
