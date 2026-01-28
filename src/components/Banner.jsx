888d2a9e04626f43cc8e50680d7fc04463dc7ec4
sagar singh <sagarsinghkoranga@gmail.com>
2026-01-28 15:28:47 +0530

    banner1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { api } from "../services/api";
 
 export default function Banners() {
   const [banners, setBanners] = useState([]);
